c23eaf3cde0f5c995093f3809b2ae74a4c98a7a0
Mehul Bawadia <mehulbawadia@gmail.com>
2021-03-05 16:58:34 +0530

    Added Products Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { GlobalStyle } from "./globalStyles";
 import Hero from "./components/Hero";
+import Products from "./components/Products";
+import { productData } from "./components/Products/data";
 
 function App() {
   return (
     <Router basename={window.location.pathname || ""}>
       <GlobalStyle />
       <Hero />
+      <Products heading="Choose Your Favorite" data={productData} />
     </Router>
   );
 }
